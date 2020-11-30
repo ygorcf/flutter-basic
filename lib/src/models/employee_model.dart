fb40e4a53755b93ef7673884c89e0448637ce465
Ygor Cruz <ygor.cruz@cocobambu.com>
2020-11-30 12:53:03 -0300

    Init

---
+++ lib/src/models/employee_model.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+List<Employee> employeesFromJson(String str) => List<Employee>.from(
+    json.decode(str).map((item) => Employee.fromJson(item)));
+
+String employeesToJson(List<Employee> data) =>
+    json.encode(List<dynamic>.from(data.map((item) => item.toJson())));
+
+class Employee {
+  int id;
+  String email;
+  String firstName;
+  String lastName;
+  String avatar;
+
+  Employee({this.id, this.email, this.firstName, this.lastName, this.avatar});
+
+  factory Employee.fromJson(Map<String, dynamic> json) => Employee(
+        id: json['id'],
+        email: json['email'],
+        firstName: json['firstName'],
+        lastName: json['lastName'],
+        avatar: json['avatar'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'email': email,
+        'firstName': firstName,
+        'lastName': lastName,
+        'avatar': avatar,
+      };
+}
---
+++ lib/src/providers/db_provider.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+import 'package:flutter_basic/src/models/employee_model.dart';
+
+class DBProvider {
+  static Database _database;
+  static final DBProvider db = DBProvider._();
+
+  DBProvider._();
+
+  Future<Database> get database async {
+    if (_database == null) {
+      _database = await initDB();
+    }
+
+    return _database;
+  }
+
+  initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, 'employee_manager.db');
+
+    return await openDatabase(path, version: 1, onOpen: (db) {},
+        onCreate: (Database db, int version) async {
+      await db.execute('CREATE TABLE Employee('
+          'id INTEGER PRIMARY KEY'
+          'email TEXT'
+          'first_name TEXT'
+          'last_name TEXT'
+          'avatar TEXT'
+          ')');
+    });
+  }
+}
